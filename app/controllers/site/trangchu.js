564d4129a696e425f3b479f814f57234c424ec81
giagocgiasoc <giagocgiasoc@gmail.com>
2020-04-12 11:28:11 +0700

    update

--- app/controllers/site/trangchu.js
+++ app/controllers/site/trangchu.js
@@ -7,28 +7,8 @@ const useragent = require('express-useragent');
 const Post = require("../../models/posts");
 
 exports.trangchu_get_home = (req, res, next) => {
-    Danhmuc.find()
-        .select("_id name link alt index image")
-        .sort('index')
-        .exec()
-        .then(docs => {
-            var danhmucall = {
-              count: docs.length,
-              danhmucs: docs.map(doc => {
-                return {
-                  name: doc.name,
-                  link:doc.link,
-                  alt:doc.alt,
-                  image: doc.image,
-                  _id: doc._id,
-                  index:doc.index,
-                  request: {
-                    type: "GET",
-                    url: "http://localhost:3000/camera/" + doc._id
-                  }
-                }
-              })
-            };
-            res.render('fontend/trangchu',{danhmucall:danhmucall,layout:'layouts/layoutadmins'});
-        })  
+
+
+            res.render('fontend/trangchu',{layout:'layouts/layoutadmins'});
+        })
   }

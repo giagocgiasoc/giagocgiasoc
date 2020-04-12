43cee7ef1c4cd8b97a5f3bdeac4cfc370c316336
giagocgiasoc <giagocgiasoc@gmail.com>
2020-04-12 11:51:14 +0700

    update

--- app/routersSite/trangchu.js
+++ app/routersSite/trangchu.js
@@ -7,29 +7,8 @@ var Cart = require('../models/cart');
  var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //router.get('/',DanhmucController.trangchu_get_home);
 router.get('/',(req, res, next) => {
-  Danhmuc.find()
-      .select("_id name link alt index image")
-      .sort('index')
-      .exec()
-      .then(docs => {
-          var danhmucall = {
-            count: docs.length,
-            danhmucs: docs.map(doc => {
-              return {
-                name: doc.name,
-                link:doc.link,
-                alt:doc.alt,
-                image: doc.image,
-                _id: doc._id,
-                index:doc.index,
-                request: {
-                  type: "GET",
-                  url: "http://localhost:3000/camera/" + doc._id
-                }
-              }
-            })
-          };
+
  res.render('fontend/test');
-})
+
 })
 module.exports = router;

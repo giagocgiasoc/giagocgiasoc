9577bf940f05040011b25111b303d18404bed618
giagocgiasoc <giagocgiasoc@gmail.com>
2020-04-12 11:46:14 +0700

    update

--- app/controllers/site/trangchu.js
+++ app/controllers/site/trangchu.js
@@ -7,6 +7,28 @@ const useragent = require('express-useragent');
 const Post = require("../../models/posts");
 
 exports.trangchu_get_home = (req, res, next) => {
-
-
-            res.render('fontend/trangchu');}
+    Danhmuc.find()
+        .select("_id name link alt index image")
+        .sort('index')
+        .exec()
+        .then(docs => {
+            var danhmucall = {
+              count: docs.length,
+              danhmucs: docs.map(doc => {
+                return {
+                  name: doc.name,
+                  link:doc.link,
+                  alt:doc.alt,
+                  image: doc.image,
+                  _id: doc._id,
+                  index:doc.index,
+                  request: {
+                    type: "GET",
+                    url: "http://localhost:3000/camera/" + doc._id
+                  }
+                }
+              })
+            };
+            res.render('fontend/trangchu',{layout:'layouts/layoutadmins'});
+        })
+  }
--- app/routersSite/trangchu.js
+++ app/routersSite/trangchu.js
@@ -7,6 +7,29 @@ var Cart = require('../models/cart');
  var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //router.get('/',DanhmucController.trangchu_get_home);
 router.get('/',(req, res, next) => {
- res.render('fontend/test');
-});
+  Danhmuc.find()
+      .select("_id name link alt index image")
+      .sort('index')
+      .exec()
+      .then(docs => {
+          var danhmucall = {
+            count: docs.length,
+            danhmucs: docs.map(doc => {
+              return {
+                name: doc.name,
+                link:doc.link,
+                alt:doc.alt,
+                image: doc.image,
+                _id: doc._id,
+                index:doc.index,
+                request: {
+                  type: "GET",
+                  url: "http://localhost:3000/camera/" + doc._id
+                }
+              }
+            })
+          };
+ res.render('fontend/test',{danhmucall:danhmucall});
+})
+})
 module.exports = router;
